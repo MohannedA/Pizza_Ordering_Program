94bbb912d9537692de967385347ba5439878521a
Mohanned Ahmed <0mohanned0@gmail.com>
2018-04-05 09:56:37 +0300

    Add Person class

---
+++ Pizza_Ordering_System/src/pizza_ordering_system/Person.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pizza_ordering_system;
+
+/**
+ *
+ * @author Mohanned Ahmed
+ */
+public class Person {
+    String name; // Person's name 
+    String phoneNumber; // Person's phone number
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    
+}
