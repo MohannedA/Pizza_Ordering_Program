7bbea04c457be82f061aab0daa1c3b78d1fecb9c
Mohanned Ahmed <0mohanned0@gmail.com>
2018-04-14 19:30:42 +0300

    Add menus files.

--- SE211Project/src/OrderHandler.java
+++ SE211Project/src/OrderHandler.java
@@ -5,13 +5,6 @@ import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author Mohanned Ahmed
