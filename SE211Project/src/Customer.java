5377af981089da17d602f2b5d10bd6b2eaf9fb06
Mohanned Ahmed <0mohanned0@gmail.com>
2018-04-14 12:36:05 +0300

    Delete Person class.

--- SE211Project/src/Customer.java
+++ SE211Project/src/Customer.java
@@ -1,14 +1,34 @@
 /**
- *
- * @author Mohanned Ahmed
+ * 
+ * @author Mohanned Ahmed - 215110114
  */
-public class Customer extends Person {
+public class Customer {
+    private String name; // Customer's name 
+    private String phoneNumber; // Customer's phone number
     private int ID; // Customer's ID 
-    private int area; // Customer's area. 
+    private int area; // Customer's area
 
-    public Customer(int ID, int area) {
+    public Customer(String name, String phoneNumber, int ID, int area) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
         this.ID = ID;
         this.area = area;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
 
     public int getID() {
@@ -25,5 +45,5 @@ public class Customer extends Person {
 
     public void setArea(int area) {
         this.area = area;
-    }    
+    }
 }
